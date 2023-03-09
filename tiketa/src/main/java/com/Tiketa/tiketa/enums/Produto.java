8f1c7dc8ac7a58bbdfed0a57239f3e8845675a93
adnescota <adnescota@hotmail.com>
2023-03-08 22:35:19 -0300

    mais ajustes

---
+++ tiketa/src/main/java/com/Tiketa/tiketa/enums/Produto.java
@@ -0,0 +1,4 @@
+package com.Tiketa.tiketa.enums;
+
+public enum Produto {
+}
--- tiketa/src/main/java/com/Tiketa/tiketa/model/Produtor.java
+++ tiketa/src/main/java/com/Tiketa/tiketa/model/Produtor.java
@@ -1,30 +1,30 @@
 package com.Tiketa.tiketa.model;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.Hibernate;
 
 import java.util.Objects;
 
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
+@Table(name ="TB_PRODUTOR")
 public class Produtor {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "int")
     private Long id;
-    @Column
+    @Column(nullable = false, length = 150)
     private String nome;
-    @Column
-
+    @Column(nullable = false, length = 150)
     private String email;
-    @Column
+    @Column(nullable = false, length = 15)
     private String telefone;
-    @Column
+    @Column(nullable = false, length = 14)
     private String cpf;
 
 }
